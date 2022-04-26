0ce1ecf819f8e4b5df2b872128c6cf28dad91179
allyssaangell <a.angell2@yahoo.com>
2022-04-25 20:09:37 -0500

    index

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,10 +3,7 @@ const commentRoutes = require('./comment-routes');
 const pizzaRoutes = require('./pizza-routes');
 
 router.use('/comments', commentRoutes);
-<<<<<<< HEAD
 // add prefix of `/pizzas` to routes created in `pizza-routes.js`
-=======
->>>>>>> develop
 router.use('/pizzas', pizzaRoutes);
 
 module.exports = router;
